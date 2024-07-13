e85f0b18479257b121db01dbdb29e238fed48124
Puskar Simkhada <puskarsimkhada22@gmail.com>
2024-07-13 11:00:32 +0545

    Changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,47 @@
-import React, { useState } from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import About from './Components/About/About'
-import Service from './Components/Service/Service'
-import Mywork from './Components/Mywork/Mywork'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
-import Privacy from './Components/Privacy/Privacy'
-import Term from './Components/Term/Term'
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
+import Service from "./Components/Service/Service";
+import Mywork from "./Components/Mywork/Mywork";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import Privacy from "./Components/Privacy/Privacy";
+import Term from "./Components/Term/Term";
 const App = () => {
+  const [showPrivacy, setshowPrivacy] = useState(false);
 
-    const [showPrivacy, setshowPrivacy] = useState(false);
-
-    const togglePrivacy = () =>{
-      setshowPrivacy(!showPrivacy);
-    };
+  const togglePrivacy = () => {
+    setshowPrivacy(!showPrivacy);
+  };
 
   const closePrivacy = () => {
     setshowPrivacy(false);
     // Restore scrolling when Privacy component is closed
-    document.body.style.overflow = 'auto';
-};
+    document.body.style.overflow = "auto";
+  };
 
-// Disable scrolling when Privacy component is open
-if (showPrivacy) {
-    document.body.style.overflow = 'hidden';
-}
+  // Disable scrolling when Privacy component is open
+  if (showPrivacy) {
+    document.body.style.overflow = "hidden";
+  }
 
+  const [showTerm, setshowTerm] = useState(false);
 
-const [showTerm, setshowTerm] = useState(false);
-
-    const toggleTerm = () =>{
-      setshowTerm(!showTerm);
-    };
+  const toggleTerm = () => {
+    setshowTerm(!showTerm);
+  };
 
   const closeTerm = () => {
     setshowTerm(false);
     // Restore scrolling when Privacy component is closed
-    document.body.style.overflow = 'auto';
-};
+    document.body.style.overflow = "auto";
+  };
 
-// Disable scrolling when Privacy component is open
-if (showTerm) {
-    document.body.style.overflow = 'hidden';
-}
-
-
+  // Disable scrolling when Privacy component is open
+  if (showTerm) {
+    document.body.style.overflow = "hidden";
+  }
 
   return (
     <div>
@@ -55,17 +51,16 @@ if (showTerm) {
       <Service></Service>
       <Mywork></Mywork>
       <Contact></Contact>
-      
+
       {/* <Privacy></Privacy> */}
       {/* <Term></Term> */}
-      <Footer togglePrivacy = {togglePrivacy} toggleTerm = {toggleTerm}></Footer>
+      <Footer togglePrivacy={togglePrivacy} toggleTerm={toggleTerm}></Footer>
       {showPrivacy && <div onClick={closePrivacy}></div>}
       {showPrivacy && <Privacy closePrivacy={closePrivacy} />}
       {showTerm && <div onClick={closeTerm}></div>}
       {showTerm && <Term closeTerm={closeTerm} />}
     </div>
-  )
+  );
 };
 
-export default App
-
+export default App;
